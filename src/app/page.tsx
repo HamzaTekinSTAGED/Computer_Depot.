b0eb203ebd434857d98c8139626e17d86660d794
HamzaTekinSTAGED <hamzatekin0973@gmail.com>
2025-02-19 19:30:31 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-[100vh] bg-blue-100 text-3xl text-pink-900">
-      <p className="flex items-center justify-center">hell yeah</p>
+      <p className="flex items-center justify-center">hell yeah31</p>
     </div>
   );
 }

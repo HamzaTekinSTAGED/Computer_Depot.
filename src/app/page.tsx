fb065e81236ef0124bfab7672e1e75ea733c61af
HamzaTekinSTAGED <hamzatekin0973@gmail.com>
2025-02-19 19:27:28 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-[100vh] bg-blue-100 text-3xl text-pink-900">
+      <p className="flex items-center justify-center">hell yeah</p>
+    </div>
+  );
+}
